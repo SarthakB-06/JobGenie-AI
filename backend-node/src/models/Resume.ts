b97ed359b09d0efe982aae4817dd1fa01e03d164
Sarthak Bansal <SarthakBansal725@gmail.com>
2026-01-02 00:13:58 +0530

    worspace modified and mongoDB connected and typescript connected

---
+++ backend-node/src/models/Resume.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+const resumeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    fileUrl: {
+        type: String, // Path to file (local or S3)
+        required: true,
+    },
+    atsScore: {
+        type: Number,
+        default: 0,
+    },
+    analysisResults: {
+        type: Object, // Stores the JSON feedback from Python AI
+        default: {},
+    },
+    isParsed: {
+        type: Boolean,
+        default: false,
+    }
+}, {
+    timestamps: true,
+})
+
+const Resume = mongoose.model('Resume', resumeSchema)
+
+export default Resume
